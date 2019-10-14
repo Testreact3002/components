a02f58f68c56397bac068d72f9657873a9ec7025
sasha <testreact2@yandex.ru>
2019-10-14 11:14:21 +0300

    typo

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import Board from "Board";
 import ChessKnight from "ChessKnight";
 import SeaBoard from "SeaBoard";
 import SeaFish from "SeaFish";
-import appSea from "appSea";
+import appSea from "seaApp";
 import BinaryToDecimal from "BinaryToDecimal";
 module.exports = {
   Board,
   ChessKnight,	
   SeaBoard,
   SeaFish,
-  appSea,
+  seaApp,
   BinaryToDecimal
 };
